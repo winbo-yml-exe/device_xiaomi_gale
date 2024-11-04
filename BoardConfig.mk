8cb238cafbca46e67888ebd78006a71be8f3f8bb
Christopher Ferris <cferris@google.com>
2024-11-04 12:45:26 -0500

    gale: Enable 32 bit building, but only 64 bit zygote
    Bug: 229787331
    Test: Build/Boot in progress.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,6 +39,9 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 
+# Enable 64-bit for non-zygote.
+ZYGOTE_FORCE_64 := true
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := gale
 TARGET_NO_BOOTLOADER := true
