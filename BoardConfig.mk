7f99caefb2b4e00d37feb48e1838d7f5c300d597
SzuWei Lin <szuweilin@google.com>
2024-11-04 12:59:20 -0500

    gale: Support dynamic 64-bit drmserver
    64-bit GSI requires support both 64_32-bit and 64-bit devices at
    the same time. Add a new variable TARGET_DYNAMIC_64_32_DRMSERVER
    to put drmserver_dynamic.rc into GSI. Which starts 64-bit
    drmserver if ro.zygote is zygote64. Otherwise, it keeps the
    original behavior to start 32-bit drmserver.
    
    Bug: 282603373
    Test: make gsi_arm64-user; Check system/etc/init
    Test: make gsi_arm-user; Check systen/etc/init

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,6 +45,9 @@ ZYGOTE_FORCE_64 := true
 # Include 64-bit mediaserver to support 64-bit only devices
 TARGET_DYNAMIC_64_32_MEDIASERVER := true
 
+# Include 64-bit drmserver to support 64-bit only devices
+TARGET_DYNAMIC_64_32_DRMSERVER := true
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := gale
 TARGET_NO_BOOTLOADER := true
